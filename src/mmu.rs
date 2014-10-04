c609ac782934a47a9e3af5bd7163f32ea8d4f2b8
Zac Stewart <zgstewart@gmail.com>
2014-10-04 14:25:49 -0400

    Use new range pattern syntax

--- src/mmu.rs
+++ src/mmu.rs
@@ -25,16 +25,16 @@ impl MMU {
   pub fn read_byte(&self, address: u16) -> u8 {
     let address = address as uint;
     match address {
-      0x0000..0x3fff => self.program[address], // ROM Bank 0
-      0x4000..0x7fff => self.program[address], // ROM Bank 1
-      0x8000..0x9fff => 0, // GPU vram
-      0xa000..0xbfff => 0, // External RAM
-      0xc000..0xdfff => self.working_ram[address & 0x1fff],
-      0xe000..0xfdff => self.working_ram[address & 0x1fff],
-      0xfe00..0xfe9f => 0, // Sprite info
-      0xfea0..0xfeff => 0,
-      0xff00..0xff7f => 0, // Memory-mapped I/O
-      0xff80..0xffff => 0, // Zero-page RAM
+      0x0000...0x3fff => self.program[address], // ROM Bank 0
+      0x4000...0x7fff => self.program[address], // ROM Bank 1
+      0x8000...0x9fff => 0, // GPU vram
+      0xa000...0xbfff => 0, // External RAM
+      0xc000...0xdfff => self.working_ram[address & 0x1fff],
+      0xe000...0xfdff => self.working_ram[address & 0x1fff], // Shadow RAM
+      0xfe00...0xfe9f => 0, // Sprite info
+      0xfea0...0xfeff => 0,
+      0xff00...0xff7f => 0, // Memory-mapped I/O
+      0xff80...0xffff => 0, // Zero-page RAM
       _ => { fail!("Read memory out of bounds: {}", address) }
     }
   }
@@ -47,16 +47,16 @@ impl MMU {
     println!("Writing {} = {}", address, value);
     let address = address as uint;
     match address {
-      0x0000..0x3fff => {}, // ROM Bank 0
-      0x4000..0x7fff => {}, // ROM Bank 1
-      0x8000..0x9fff => {}, // GPU vram
-      0xa000..0xbfff => {}, // External RAM
-      0xc000..0xdfff => self.working_ram[address & 0x1fff] = value,
-      0xe000..0xfdff => self.working_ram[address & 0x1fff] = value,
-      0xfe00..0xfe9f => {}, // Sprite info
-      0xff00..0xff7f => {}, // Memory-mapped I/O
-      0xff80..0xffff => {}, // Zero-page RAM
-      _ => { fail!("Wrote memory out of bounds") }
+      0x0000...0x3fff => {}, // ROM Bank 0
+      0x4000...0x7fff => {}, // ROM Bank 1
+      0x8000...0x9fff => {}, // GPU vram
+      0xa000...0xbfff => {}, // External RAM
+      0xc000...0xdfff => self.working_ram[address & 0x1fff] = value,
+      0xe000...0xfdff => self.working_ram[address & 0x1fff] = value, // Shadow RAM
+      0xfe00...0xfe9f => {}, // Sprite info
+      0xff00...0xff7f => {}, // Memory-mapped I/O
+      0xff80...0xffff => {}, // Zero-page RAM
+      _ => { fail!("Wrote memory out of bounds: {}", address) }
     }
   }
 }
