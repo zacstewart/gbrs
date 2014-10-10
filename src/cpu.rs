3404c956a2a84fb8da4e3ec94c9a46716e80c1f0
Zac Stewart <zgstewart@gmail.com>
2014-10-10 13:40:35 -0400

    Modularize opcodes
    Try to reuse loads and such instead of writing a function to correspond to each op

--- src/cpu.rs
+++ src/cpu.rs
@@ -129,60 +129,60 @@ impl CPU {
 
   pub fn execute(&mut self, op: u8) {
     match op {
-      0x00 => self.NOP(),
-      0x01 => self.LD_bc_d16(),
-      0x02 => self.LD_mbc_a(),
-      0x03 => self.INC_bc(),
-      0x04 => self.INC_b(),
-      0x05 => self.DEC_b(),
-      0x06 => self.LD_b_d8(),
-      0x07 => self.RLCA(),
-      0x08 => self.LD_a16_sp(),
-      0x09 => self.ADD_hl_bc(),
-      0x0a => self.LD_a_mbc(),
-      0x0b => self.DEC_bc(),
-      0x0c => self.INC_c(),
-      0x0d => self.DEC_c(),
-      0x0e => self.LD_c_d8(),
-      0x0f => self.RRCA(),
-      0x10 => self.STOP(),
-      0x11 => self.LD_de_d16(),
-      0x12 => self.LD_mde_a(),
-      0x13 => self.INC_de(),
-      0x14 => self.INC_d(),
-      0x15 => self.DEC_d(),
-      0x16 => self.LD_d_d8(),
-      0x17 => self.RLA(),
+      0x00 => self.nop(),
+      0x01 => { let v = self.immediate_word(); self.ld_bc(v); },
+      0x02 => { let am = self.address_bc(); self.ld_mem_a(am); },
+      0x03 => self.inc_bc(),
+      0x04 => self.inc_b(),
+      0x05 => self.dec_b(),
+      0x06 => { let v = self.immediate(); self.ld_b(v); },
+      0x07 => self.rlca(),
+      0x08 => { let am = self.immediate_word(); self.ld_mem_sp(am); },
+      0x09 => self.add_hl_bc(),
+      0x0a => { let am = self.address_bc(); self.ld_a(am); },
+      0x0b => self.dec_bc(),
+      0x0c => self.inc_c(),
+      0x0d => self.dec_c(),
+      0x0e => { let am = self.immediate(); self.ld_c(am); },
+      0x0f => self.rrca(),
+      0x10 => self.stop(),
+      0x11 => { let am = self.immediate_word(); self.ld_de(am) },
+      0x12 => { let am = self.address_de(); self.ld_mem_a(am); },
+      0x13 => self.inc_de(),
+      0x14 => self.inc_d(),
+      0x15 => self.dec_d(),
+      0x16 => { let am = self.immediate(); self.ld_d(am); },
+      0x17 => self.rla(),
       0x18 => { let v = self.immediate(); self.jr(v) },
-      0x19 => self.ADD_hl_de(),
-      0x1a => self.LD_a_mde(),
-      0x1b => self.DEC_de(),
-      0x1c => self.INC_e(),
-      0x1d => self.DEC_e(),
-      0x1e => self.LD_e_d8(),
-      0x1f => self.RRA(),
+      0x19 => self.add_hl_de(),
+      0x1a => { let am = self.address_de(); self.ld_a(am); },
+      0x1b => self.dec_de(),
+      0x1c => self.inc_e(),
+      0x1d => self.dec_e(),
+      0x1e => { let am = self.immediate(); self.ld_e(am); },
+      0x1f => self.rra(),
       0x20 => { let v = self.immediate(); self.jr_nz(v); },
-      0x21 => self.LD_hl_d16(),
-      0x23 => self.INC_hl(),
-      0x24 => self.INC_h(),
-      0x26 => self.LD_h_d8(),
+      0x21 => { let am = self.immediate_word(); self.ld_hl(am); },
+      0x23 => self.inc_hl(),
+      0x24 => self.inc_h(),
+      0x26 => { let am = self.immediate(); self.ld_h(am); },
       0x28 => { let v = self.immediate(); self.jr_z(v); },
-      0x29 => self.ADD_hl_hl(),
-      0x2a => self.LD_a_mhli(),
-      0x2c => self.INC_l(),
-      0x2d => self.DEC_l(),
-      0x2e => self.LD_l_d8(),
+      0x29 => self.add_hl_hl(),
+      0x2a => { let am = self.address_hli(); self.ld_a(am); },
+      0x2c => self.inc_l(),
+      0x2d => self.dec_l(),
+      0x2e => { let am = self.immediate(); self.ld_l(am); },
       0x30 => { let v = self.immediate(); self.jr_nc(v); },
-      0x31 => self.LD_sp_d16(),
-      0x33 => self.INC_sp(),
+      0x31 => { let am = self.immediate_word(); self.ld_sp(am); },
+      0x33 => self.inc_sp(),
       0x38 => { let v = self.immediate(); self.jr_c(v); },
-      0x39 => self.ADD_hl_sp(),
-      0x3a => self.LD_a_mhld(),
-      0x3c => self.INC_a(),
-      0x3d => self.DEC_a(),
-      0x3e => self.LD_a_d8(),
-      0x76 => self.HALT(),
-      e => println!("{}", self)
+      0x39 => self.add_hl_sp(),
+      0x3a => { let am = self.address_hld(); self.ld_a(am); },
+      0x3c => self.inc_a(),
+      0x3d => self.dec_a(),
+      0x3e => { let am = self.immediate(); self.ld_a(am); },
+      0x76 => self.halt(),
+      _ => println!("{}", self)
     }
     self.clock.m += self.m;
   }
@@ -214,6 +214,23 @@ impl CPU {
 
   fn address_de(&mut self) -> MemoryAddressingMode {
     let address = (self.d as u16 << 8) + self.e as u16;
+    self.address(address)
+  }
+
+  fn address_hl(&mut self) -> MemoryAddressingMode {
+    let address = (self.h as u16 << 8) + self.l as u16;
+    self.address(address)
+  }
+
+  fn address_hli(&mut self) -> MemoryAddressingMode {
+    let address = (self.h as u16 << 8) + self.l as u16;
+    self.inc_hl();
+    self.address(address)
+  }
+
+  fn address_hld(&mut self) -> MemoryAddressingMode {
+    let address = (self.h as u16 << 8) + self.l as u16;
+    self.dec_hl();
     self.address(address)
   }
 
@@ -275,6 +292,16 @@ impl CPU {
     }
   }
 
+  fn ld_de<AM:AddressingMode>(&mut self, am: AM) {
+    match am.load(self) {
+      Word(word) => {
+        self.e = get_upper_bytes(word);
+        self.d = get_lower_bytes(word);
+      },
+      _ => fail!("Unexpected addressing mode")
+    }
+  }
+
   // Stores
 
   fn st_a<AM:AddressingMode>(&mut self, am: AM) {
@@ -282,12 +309,17 @@ impl CPU {
     am.store(self, a);
   }
 
+  fn st_sp<AM:AddressingMode>(&mut self, am: AM) {
+    let value = Word(self.sp);
+    am.store(self, value);
+  }
+
   // Arithmetic
 
   fn add_hl(&mut self, value: u16) {
     let mut hl = (self.h << 8) as u16 + self.l as u16;
 
-    if (hl + value < hl) {
+    if hl + value < hl {
       self.flags.c = true;
     } else {
       self.flags.c = false;
@@ -322,81 +354,37 @@ impl CPU {
 
   // Ops
 
-  fn NOP(&mut self) {
+  fn nop(&mut self) {
     self.m = 1;
     self.t = 4;
   }
 
-  fn LD_bc_d16(&mut self) {
-    let v = self.immediate();
-    self.ld_c(v);
-    let v = self.immediate();
-    self.ld_b(v);
-    self.m = 3;
-  }
-
-  fn LD_de_d16(&mut self) {
-    let v = self.immediate();
-    self.ld_e(v);
-    let v = self.immediate();
-    self.ld_d(v);
-    self.m = 3;
-  }
-
-  fn LD_hl_d16(&mut self) {
-    let v = self.immediate();
-    self.ld_l(v);
-    let v = self.immediate();
-    self.ld_h(v);
-    self.m = 3;
-  }
-
-  fn LD_sp_d16(&mut self) {
-    let v = self.immediate_word();
-    match v.load(self) {
-      Word(word) => self.sp = word,
-      _ => {}
+  fn ld_bc<AM:AddressingMode>(&mut self, am: AM) {
+    match am.load(self) {
+      Word(word) => {
+        self.b = get_upper_bytes(word);
+        self.c = get_lower_bytes(word);
+      },
+      _ => fail!("Unexpected addressing mode")
     }
-    self.m = 3;
   }
 
-  fn LD_mbc_a(&mut self) {
-    let am = self.address_bc();
-    self.st_a(am);
-    self.m = 2;
+  fn ld_hl<AM:AddressingMode>(&mut self, am: AM) {
+    match am.load(self) {
+      Word(word) => {
+        self.h = get_upper_bytes(word);
+        self.l = get_lower_bytes(word);
+      },
+      _ => fail!("Unexpected addressing mode")
+    }
   }
 
-  fn LD_mde_a(&mut self) {
-    let am = self.address_de();
-    self.st_a(am);
-    self.m = 2;
+  fn ld_mem_a<AM:AddressingMode>(&mut self, am: AM) {
+    let data = Byte(self.a);
+    am.store(self, data);
   }
 
-  fn LD_a_mbc(&mut self) {
-    let am = self.address_bc();
-    self.ld_a(am);
-    self.m = 2;
-  }
-
-  fn LD_a_mde(&mut self) {
-    let am = self.address_de();
-    self.ld_a(am);
-    self.m = 2;
-  }
-
-  fn LD_a_mhli(&mut self) {
-    let am = self.address_de();
-    self.ld_a(am);
-    self.inc(am)
-  }
-
-  fn LD_a_mhld(&mut self) {
-    let am = self.address_de();
-    self.ld_a(am);
-    self.dec(am)
-  }
-
-  fn INC_bc(&mut self) {
+  fn inc_bc(&mut self) {
     if self.c == 255 {
       self.b += 1;
     }
@@ -404,7 +392,7 @@ impl CPU {
     self.m = 1;
   }
 
-  fn INC_de(&mut self) {
+  fn inc_de(&mut self) {
     if self.e == 255 {
       self.d += 1;
     }
@@ -412,7 +400,7 @@ impl CPU {
     self.m = 1;
   }
 
-  fn INC_hl(&mut self) {
+  fn inc_hl(&mut self) {
     if self.l == 255 {
       self.h += 1;
     }
@@ -420,12 +408,12 @@ impl CPU {
     self.m = 1;
   }
 
-  fn INC_sp(&mut self) {
+  fn inc_sp(&mut self) {
     self.sp += 1;
     self.m = 1;
   }
 
-  fn INC_b(&mut self) {
+  fn inc_b(&mut self) {
     self.b += 1;
     if self.b == 0 {
       self.flags.z = true;
@@ -434,7 +422,7 @@ impl CPU {
     }
   }
 
-  fn INC_c(&mut self) {
+  fn inc_c(&mut self) {
     self.c += 1;
     if self.c == 0 {
       self.flags.z = true;
@@ -443,7 +431,7 @@ impl CPU {
     }
   }
 
-  fn INC_d(&mut self) {
+  fn inc_d(&mut self) {
     self.d += 1;
     if self.d == 0 {
       self.flags.z = true;
@@ -452,7 +440,7 @@ impl CPU {
     }
   }
 
-  fn INC_e(&mut self) {
+  fn inc_e(&mut self) {
     self.e += 1;
     if self.e == 0 {
       self.flags.z = true;
@@ -461,7 +449,7 @@ impl CPU {
     }
   }
 
-  fn INC_h(&mut self) {
+  fn inc_h(&mut self) {
     self.h += 1;
     if self.h == 0 {
       self.flags.z = true;
@@ -470,7 +458,7 @@ impl CPU {
     }
   }
 
-  fn INC_l(&mut self) {
+  fn inc_l(&mut self) {
     self.l += 1;
     if self.l == 0 {
       self.flags.z = true;
@@ -479,7 +467,7 @@ impl CPU {
     }
   }
 
-  fn INC_a(&mut self) {
+  fn inc_a(&mut self) {
     self.a += 1;
     if self.a == 0 {
       self.flags.z = true;
@@ -488,7 +476,7 @@ impl CPU {
     }
   }
 
-  fn DEC_bc(&mut self) {
+  fn dec_bc(&mut self) {
     if self.c == 0 {
       self.b -= 1
     }
@@ -496,7 +484,7 @@ impl CPU {
     self.m = 8;
   }
 
-  fn DEC_de(&mut self) {
+  fn dec_de(&mut self) {
     if self.e == 0 {
       self.d -= 1
     }
@@ -504,7 +492,7 @@ impl CPU {
     self.m = 8;
   }
 
-  fn DEC_hl(&mut self) {
+  fn dec_hl(&mut self) {
     if self.l == 0 {
       self.h -= 1
     }
@@ -512,12 +500,12 @@ impl CPU {
     self.m = 8;
   }
 
-  fn DEC_sp(&mut self) {
+  fn dec_sp(&mut self) {
     self.sp -= 1;
     self.m = 8;
   }
 
-  fn DEC_b(&mut self) {
+  fn dec_b(&mut self) {
     self.b -= 1;
     if self.b == 0 {
       self.flags.z = true;
@@ -526,7 +514,7 @@ impl CPU {
     }
   }
 
-  fn DEC_c(&mut self) {
+  fn dec_c(&mut self) {
     self.c -= 1;
     if self.c == 0 {
       self.flags.z = true;
@@ -535,7 +523,7 @@ impl CPU {
     }
   }
 
-  fn DEC_d(&mut self) {
+  fn dec_d(&mut self) {
     self.d -= 1;
     if self.d  == 0 {
       self.flags.z = true;
@@ -544,7 +532,7 @@ impl CPU {
     }
   }
 
-  fn DEC_e(&mut self) {
+  fn dec_e(&mut self) {
     self.e -= 1;
     if self.e  == 0 {
       self.flags.z = true;
@@ -553,7 +541,7 @@ impl CPU {
     }
   }
 
-  fn DEC_l(&mut self) {
+  fn dec_l(&mut self) {
     self.l -= 1;
     if self.l  == 0 {
       self.flags.z = true;
@@ -562,7 +550,7 @@ impl CPU {
     }
   }
 
-  fn DEC_a(&mut self) {
+  fn dec_a(&mut self) {
     self.a -= 1;
     if self.a  == 0 {
       self.flags.z = true;
@@ -571,49 +559,7 @@ impl CPU {
     }
   }
 
-  fn LD_b_d8(&mut self) {
-    let v = self.immediate();
-    self.ld_b(v);
-    self.m = 2;
-  }
-
-  fn LD_c_d8(&mut self) {
-    let v = self.immediate();
-    self.ld_c(v);
-    self.m = 2;
-  }
-
-  fn LD_d_d8(&mut self) {
-    let v = self.immediate();
-    self.ld_d(v);
-    self.m = 2;
-  }
-
-  fn LD_e_d8(&mut self) {
-    let v = self.immediate();
-    self.ld_e(v);
-    self.m = 2;
-  }
-
-  fn LD_h_d8(&mut self) {
-    let v = self.immediate();
-    self.ld_h(v);
-    self.m = 2;
-  }
-
-  fn LD_l_d8(&mut self) {
-    let v = self.immediate();
-    self.ld_l(v);
-    self.m = 2;
-  }
-
-  fn LD_a_d8(&mut self) {
-    let v = self.immediate();
-    self.ld_a(v);
-    self.m = 2;
-  }
-
-  fn RLCA(&mut self) {
+  fn rlca(&mut self) {
     // put bit 7 of a in carry flag
     if (self.a & 0x80) == 0x80 {
       self.flags.c = true
@@ -625,7 +571,7 @@ impl CPU {
     self.m = 4;
   }
 
-  fn RLA(&mut self) {
+  fn rla(&mut self) {
     let old_f: u8;
     if self.flags.c {
       old_f = 1;
@@ -643,7 +589,7 @@ impl CPU {
     self.m = 4;
   }
 
-  fn RRCA(&mut self) {
+  fn rrca(&mut self) {
     if (self.a & 1) == 1 {
       self.flags.c = true;
     } else {
@@ -653,7 +599,7 @@ impl CPU {
     self.m = 4;
   }
 
-  fn RRA(&mut self) {
+  fn rra(&mut self) {
     let old_f: u8;
     if self.flags.c {
       old_f = 0x80;
@@ -725,42 +671,49 @@ impl CPU {
     }
   }
 
-  fn LD_a16_sp(&mut self) {
-    match self.immediate_word().load(self) {
+  fn ld_mem_sp<AM:AddressingMode>(&mut self, am: AM) {
+    match am.load(self) {
       Word(word) => {
         let am = self.address(word);
-        self.ld_sp(am)
+        self.st_sp(am)
       },
       _ => {}
     }
     self.m = 3;
   }
 
-  fn ADD_hl_bc(&mut self) {
+  fn add_hl_bc(&mut self) {
     let bc = (self.b << 8) as u16 + self.c as u16;
     self.add_hl(bc);
   }
 
-  fn ADD_hl_de(&mut self) {
+  fn add_hl_de(&mut self) {
     let de = (self.d << 8) as u16 + self.e as u16;
     self.add_hl(de);
   }
 
-  fn ADD_hl_hl(&mut self) {
+  fn add_hl_hl(&mut self) {
     let hl = (self.h << 8) as u16 + self.l as u16;
     self.add_hl(hl);
   }
 
-  fn ADD_hl_sp(&mut self) {
+  fn add_hl_sp(&mut self) {
     let sp = self.sp;
     self.add_hl(sp);
   }
 
-  fn HALT(&mut self) {
+  fn halt(&mut self) {
     //fail!("HALT");
   }
 
-  fn STOP(&mut self) {
+  fn stop(&mut self) {
     self.pc += 1;
   }
+}
+
+fn get_lower_bytes(word: u16) -> u8 {
+  (word & 0xff) as u8
+}
+fn get_upper_bytes(word: u16) -> u8 {
+  ((word & 0xff00) >> 8) as u8
 }
