22602c9d2e2255c821abd51a5ed532aba93fe3dd
Zac Stewart <zgstewart@gmail.com>
2015-08-13 09:12:40 -0400

    Route external RAM address space to Cartridge

--- src/mmu.rs
+++ src/mmu.rs
@@ -41,7 +41,6 @@ impl MMU {
 impl ReadByte for MMU {
   fn read_byte(&self, address: u16) -> u8 {
     match address {
-      0xa000...0xbfff => 0, // External RAM
       0xc000...0xdfff => self.working_ram[(address & 0x1fff) as usize],
       0xe000...0xfdff => self.working_ram[(address & 0x1fff) as usize], // Shadow RAM
       0xfe00...0xfe9f => 0, // Sprite info
@@ -50,6 +49,7 @@ impl ReadByte for MMU {
       0xff40...0xff7f => { self.gpu.read_byte(address) } // GPU
       0x0000...0x7fff => { self.cartridge.read_byte(address) }              // ROM bank 0 & switchable [Cartridge]
       0x8000...0x9fff => { self.gpu.read_byte(address) },                   // VRAM [GPU]
+      0xa000...0xbfff => { self.cartridge.read_byte(address) }              // External RAM [Cartridge]
       0xff80...0xfffe => { self.hram[(address & 0x7f) as usize] }           // Zero-page RAM (High RAM, HRAM)
       0xffff => { 0 }                                                       // Interrupt enable register
       _ => { panic!("Read memory out of bounds: {}", address) }
@@ -61,7 +61,6 @@ impl WriteByte for MMU {
   fn write_byte(&mut self, address: u16, value: u8) {
     //println!("Writing {:x} = {:x}", address, value);
     match address {
-      0xa000...0xbfff => {}, // External RAM
       0xc000...0xdfff => self.working_ram[(address & 0x1fff) as usize] = value,
       0xe000...0xfdff => self.working_ram[(address & 0x1fff) as usize] = value, // Shadow RAM
       0xfe00...0xfe9f => {}, // Sprite info
@@ -70,6 +69,7 @@ impl WriteByte for MMU {
       _ => { panic!("Wrote memory out of bounds: {}", address) }
       0x0000...0x7fff => { self.cartridge.write_byte(address, value); },                // ROM Bank 0 & switchable [Cartridge]
       0x8000...0x9fff => { self.gpu.write_byte(address, value); }                       // VRAM [GPU]
+      0xa000...0xbfff => { self.cartridge.write_byte(address, value); }                 // External RAM [Cartridge]
       0xff80...0xfffe => { self.hram[(address & 0x7f) as usize] = value }               // Zero-page RAM (High RAM, HRAM)
       0xffff => { println!("Write to interrupt enable register: {:2x} = {:2x}", address, value) } // Interrupt enable register
     }
