9dd952768689616b4dde40a5b31800c2a6e05b7a
Zac Stewart <zgstewart@gmail.com>
2015-08-15 10:19:40 -0400

    Implement OAM access in GPU

--- src/gpu.rs
+++ src/gpu.rs
@@ -3,6 +3,7 @@ use memory_map::{ReadByte, WriteByte};
 pub struct GPU {
     memory: [u8; 0xbf],
     vram: [u8; 8192],
+    oam: [u8; 160],
 }
 
 impl GPU {
@@ -10,6 +11,7 @@ impl GPU {
         GPU {
             memory: [0; 0xbf],
             vram: [0; 8192],
+            oam: [0; 160],
         }
     }
 }
@@ -18,6 +20,7 @@ impl ReadByte for GPU {
     fn read_byte(&self, address: u16) -> u8 {
         match address {
             0x8000...0x9fff => { self.vram[(address & 0x1fff) as usize] }
+            0xfe00...0xfe9f => { self.oam[(address & 0xff) as usize] }
             _ => { self.memory[(address - BASE) as usize] }
         }
     }
@@ -27,6 +30,7 @@ impl WriteByte for GPU {
     fn write_byte(&mut self, address: u16, value: u8) {
         match address {
             0x8000...0x9fff => { self.vram[(address & 0x1fff) as usize] = value }
+            0xfe00...0xfe9f => { self.oam[(address & 0xff) as usize] = value }
         }
     }
 }
