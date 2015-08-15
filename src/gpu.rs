4e988ee533d4c790fd96de3e2a244d6d254749bf
Zac Stewart <zgstewart@gmail.com>
2015-08-15 11:32:53 -0400

    Implement STAT in GPU

--- src/gpu.rs
+++ src/gpu.rs
@@ -2,16 +2,31 @@ use memory_map::{ReadByte, WriteByte};
 
 const BASE: u16 = 0xff40;
 
+enum LineMode {
+    HBlank = 0,
+    VBlank = 1,
+    OAMRead = 2,
+    VRAMRead = 3
+}
+
 pub struct GPU {
     memory: [u8; 0xbf],
     vram: [u8; 8192],
     oam: [u8; 160],
-    lcd_on: bool,
+
+    lcd_on: bool,               // LCDC
     bg_tile_select: u8,
     bg_map_select: u8,
     obj_size: u8,
     obj_display_enable: bool,
-    bg_display_enable: bool
+    bg_display_enable: bool,
+
+    coincidence_interrupt: u8,  // STAT
+    oam_interrupt: u8,
+    v_blank_interrupt: u8,
+    h_blank_interrupt: u8,
+    coincidence_flag: u8,
+    line_mode: LineMode
 }
 
 impl GPU {
@@ -25,7 +40,13 @@ impl GPU {
             bg_map_select: 0,
             obj_size: 0,
             obj_display_enable: false,
-            bg_display_enable: true
+            bg_display_enable: true,
+            coincidence_interrupt: 0,
+            oam_interrupt: 0,
+            v_blank_interrupt: 0,
+            h_blank_interrupt: 0,
+            coincidence_flag: 0,
+            line_mode: LineMode::OAMRead,
         }
     }
 }
@@ -45,6 +66,21 @@ impl ReadByte for GPU {
                 if self.bg_display_enable { value |= 0b00000001; }
                 value
             }
+            0xff41 => {
+                let mut value = 0;
+                value |= ((self.coincidence_interrupt & 1) << 6);
+                value |= ((self.oam_interrupt & 1) << 5);
+                value |= ((self.v_blank_interrupt & 1) << 4);
+                value |= ((self.h_blank_interrupt & 1) << 3);
+                value |= ((self.coincidence_flag & 1) << 2);
+                value |= match self.line_mode {
+                    LineMode::HBlank => 0,
+                    LineMode::VBlank => 1,
+                    LineMode::OAMRead => 2,
+                    LineMode::VRAMRead => 3
+                };
+                value
+            }
             _ => { self.memory[(address - BASE) as usize] }
         }
     }
@@ -62,6 +98,20 @@ impl WriteByte for GPU {
                 self.obj_size = (value & 0b00000100) >> 2;
                 self.obj_display_enable = (value & 0b00000010) == 0b000000010;
                 self.bg_display_enable = (value & 0b00000001) == 0b00000001;
+            }
+            0xff41 => {
+                self.coincidence_interrupt = (value & 0b0100_0000) >> 6;
+                self.oam_interrupt = (value & 0b0010_0000) >> 5;
+                self.v_blank_interrupt = (value & 0b0001_0000) >> 4;
+                self.h_blank_interrupt = (value & 0b0000_1000) >> 3;
+                self.coincidence_flag = (value & 0b0000_0100) >> 2;
+                self.line_mode = match (value & 0b0000_0011) {
+                    0 => LineMode::HBlank,
+                    1 => LineMode::VBlank,
+                    2 => LineMode::OAMRead,
+                    3 => LineMode::VRAMRead,
+                    _ => panic!("Invalid line mode: {:2x}", value)
+                };
             }
         }
     }
