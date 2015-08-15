4c16f2edb77b9eb9cdfd8148d380299dd6f7a223
Zac Stewart <zgstewart@gmail.com>
2015-08-15 10:19:09 -0400

    Implement VRAM access in GPU

--- src/gpu.rs
+++ src/gpu.rs
@@ -1,25 +1,32 @@
 use memory_map::{ReadByte, WriteByte};
 
 pub struct GPU {
-    current_line: u8
+    memory: [u8; 0xbf],
+    vram: [u8; 8192],
 }
 
 impl GPU {
     pub fn new() -> GPU {
         GPU {
-            current_line: 0
+            memory: [0; 0xbf],
+            vram: [0; 8192],
         }
     }
 }
 
 impl ReadByte for GPU {
     fn read_byte(&self, address: u16) -> u8 {
-        0
+        match address {
+            0x8000...0x9fff => { self.vram[(address & 0x1fff) as usize] }
+            _ => { self.memory[(address - BASE) as usize] }
+        }
     }
 }
 
 impl WriteByte for GPU {
     fn write_byte(&mut self, address: u16, value: u8) {
-        println!("Write to GPU: {} = {}", address, value);
+        match address {
+            0x8000...0x9fff => { self.vram[(address & 0x1fff) as usize] = value }
+        }
     }
 }
