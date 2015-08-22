eafb494a64c4805330f8a0ff8e1940db8bff3936
Zac Stewart <zgstewart@gmail.com>
2015-08-22 15:37:48 -0400

    Add external RAM to Cartridge

--- src/cartridge.rs
+++ src/cartridge.rs
@@ -3,13 +3,17 @@ use std::io::Read;
 use memory_map::{ReadByte, WriteByte};
 
 pub struct Cartridge {
-    rom: Box<[u8]>
+    rom: Box<[u8]>,
+    ram: Box<[u8]>,
+    ram_enabled: bool
 }
 
 impl Cartridge {
     pub fn new(rom: Box<[u8]>) -> Cartridge {
         Cartridge {
-            rom: rom
+            rom: rom,
+            ram: Box::new([0; 0x1fff]),
+            ram_enabled: false
         }
     }
 
@@ -31,12 +35,29 @@ impl Cartridge {
 impl ReadByte for Cartridge {
     fn read_byte(&self, address: u16) -> u8 {
         //println!("Reading cart: {:2x}", address);
-        self.rom[address as usize]
+        match address {
+            0xa000...0xbfff => {
+                if self.ram_enabled {
+                    self.ram[(address - 0xa000) as usize]
+                } else {
+                    0
+                }
+            }
+            _ => { self.rom[address as usize] }
+        }
     }
 }
 
 impl WriteByte for Cartridge {
     fn write_byte(&mut self, address: u16, value: u8) {
-        println!("Write to Cart: {:2x} = {:2x}", address, value);
+        match address {
+            0x0000...0x1fff => { self.ram_enabled = (value & 0xff) == 0x0a; }
+            0xa000...0xbfff => {
+                if self.ram_enabled {
+                    self.ram[(address - 0xa000) as usize] = value;
+                }
+            }
+            _ => { println!("Write to Cart: {:2x} = {:2x}", address, value); }
+        }
     }
 }
